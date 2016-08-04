9c9129266520cc212e34f1b08591cf05f57b622e
Rails Udemy Course <adam.butcher@tuskerdirect.com>
2016-08-04 14:37:08 +0000

    Extract redunancies and prep for prod

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,60 +1,54 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
   
   def index
     @articles = Article.all
   end
-  
-  
-  
-   def new
-       @article = Article.new
-   end
+
+  def new
+    @article = Article.new
+  end
    
-   def edit
+  def edit
+    
+  end
+   
+  #Create action and private article_params method for string parameters in the articles controller
+  def create
+      
+    @article = Article.new(article_params)
+    if @article.save
+      flash[:notice] = "Article was successfully created"      
+      redirect_to article_path(@article)
+    else
+      render 'new'
+    end
+  end
+    
+  def update
+    if @article.update(article_params)
+      flash[:notice] = "Article was successfully updated"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
+  end
+    
+  def show
+  end
+    
+  def destroy
+    @article.destroy
+    flash[:notice] = "Article was deleted"
+    redirect_to articles_path
+  end
+    
+    
+  private
+  def article_params
+      params.require(:article).permit(:title, :description)    
+  end
+  def set_article
     @article = Article.find(params[:id])
-   end
-   
-   
-    #Create action and private article_params method for string parameters in the articles controller
-    def create
-      
-      @article = Article.new(article_params)
-      
-      if @article.save
-          flash[:notice] = "Article was successfully created"      
-          redirect_to article_path(@article)
-      else
-        render 'new'
-      end
-    end
-    
-    def update
-      @article = Article.find(params[:id])  
-      if @article.update(article_params)
-        flash[:notice] = "Article was successfully updated"
-        redirect_to article_path(@article)
-      else
-        render 'edit'
-      end
-    end
-    
-    def show
-      @article = Article.find(params[:id])
-    end
-    
-    def destroy
-      @article = Article.find(params[:id]) 
-      @article.destroy
-      flash[:notice] = "Article was deleted"
-      redirect_to articles_path
-    end
-    
-    
-    private
-    def article_params
-        params.require(:article).permit(:title, :description)    
-    end
-    
-   
-    
+  end
 end
