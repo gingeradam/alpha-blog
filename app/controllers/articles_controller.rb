ce1114a5c8d9f6fe1dfaac2c918d5d28baf0021f
Rails Udemy Course <adam.butcher@tuskerdirect.com>
2016-08-04 12:59:06 +0000

    Add edit, update, show and index listing

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,19 @@
 class ArticlesController < ApplicationController
+  
+  def index
+    @articles = Article.all
+  end
+  
+  
+  
    def new
        @article = Article.new
    end
+   
+   def edit
+    @article = Article.find(params[:id])
+   end
+   
    
     #Create action and private article_params method for string parameters in the articles controller
     def create
@@ -16,6 +28,17 @@ class ArticlesController < ApplicationController
       end
     end
     
+    def update
+      
+      @article = Article.find(params[:id])  
+      if @article.update(article_params)
+        flash[:notice] = "Article was successfully updated"
+        redirect_to article_path(@article)
+      else
+        render 'edit'
+      end
+    end
+    
     def show
       @article = Article.find(params[:id])
     end
@@ -24,5 +47,7 @@ class ArticlesController < ApplicationController
     def article_params
         params.require(:article).permit(:title, :description)    
     end
+    
+   
     
 end
