41c5ebbdb84fa994952234eebc7ba0f8323ce631
Rails Udemy Course <adam.butcher@tuskerdirect.com>
2016-08-19 16:11:15 +0000

    Create show action and add gravatar image

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User <ActiveRecord::Base
   VALID_EMAIL_REGEX= /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, 
             uniqueness: { case_sensitive: false }, 
-            length: {maximum: 25},
+            length: {maximum: 250},
             format: { with: VALID_EMAIL_REGEX }
             
 has_secure_password
