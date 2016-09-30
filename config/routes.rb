65d4f15f4e2d113fc8954b19f4990b24d7058456
Rails Udemy Course <adam.butcher@tuskerdirect.com>
2016-09-30 15:32:19 +0000

    User authentication

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   resources :users, except: [:new]
   
   get 'login', to: 'sessions#new'
-  post 'login', to: 'sessions#controller'
+  post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
 
 end
