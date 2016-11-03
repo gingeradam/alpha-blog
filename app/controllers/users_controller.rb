2f85b0487cad026e464f34d3c19c645ee2355bdf
Rails Udemy Course <adam.butcher@tuskerdirect.com>
2016-11-03 14:48:07 +0000

    Add categories model and tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -57,7 +57,7 @@ class UsersController < ApplicationController
   
   def require_same_user
     if current_user != @user and !current_user.admin?
-      flash[:danger] = "Tou can only edit your own account!"
+      flash[:danger] = "You can only edit your own account!"
       redirect_to root_path
     end
   end
